8d2e53e2eb8f674752c68fc297d9a73606c602bb
Abdiladiif-Abdisamed <cabdiladiifcabdisamed@gmail.com>
2025-05-09 19:03:10 +0300

    added cookei

--- main.js
+++ main.js
@@ -63,3 +63,72 @@ document.addEventListener('scroll', function() {
 });
 
 
+// cookie
+document.addEventListener("DOMContentLoaded", () => {
+    const coockieContainer = document.querySelector(".coockie-container");
+    const contactCookie = document.querySelector(".contactCookie");
+    const ASTeamCookie = document.querySelector(".ASTeamCookie");
+    const AsTeam = document.querySelector(".AsTeam");
+    const Asindv= document.querySelector(".indv");
+    const times = document.querySelectorAll(".times");
+    const button = document.querySelector(".btn");
+    const ASind = document.querySelector(".ASind")
+    const abdiladiif = document.querySelector(".abdiladiif")
+    const abdiInfo = document.querySelector(".abdiInfo")
+    const naima = document.querySelector(".naima")
+    const naimaInfo = document.querySelector(".naimaInfo")
+  
+  
+    setTimeout(() => {
+      coockieContainer.style.display = "block";
+    }, 1000);
+  times.forEach((time) => {
+      time.addEventListener("click", () => {
+        coockieContainer.style.display = "none";
+        naimaInfo.style.display = "none";
+        ASind.style.display = "none";
+        ASTeamCookie.style.display = "none";
+        coockieContainer.style.display = "none";
+        contactCookie.style.display = "none";
+        alert("Thanks for your feedback");
+      });
+    });
+    
+  
+    button.addEventListener("click", () => {
+      contactCookie.style.display = "flex";
+      coockieContainer.style.display = "none";
+     
+    });
+  
+    AsTeam.addEventListener("click", () => {
+      ASTeamCookie.style.display = "flex";
+      coockieContainer.style.display = "none";
+      contactCookie.style.display = "none";
+     
+    });
+  
+    Asindv.addEventListener("click", ()=>{
+      ASind.style.display = "flex";
+      ASTeamCookie.style.display = "none";
+      coockieContainer.style.display = "none";
+      contactCookie.style.display = "none";
+  
+    })
+  
+    abdiladiif.addEventListener("click",()=>{
+      abdiInfo.style.display = "flex";
+      ASind.style.display = "none";
+      ASTeamCookie.style.display = "none";
+      coockieContainer.style.display = "none";
+      contactCookie.style.display = "none";
+    })
+    naima.addEventListener("click",()=>{
+      naimaInfo.style.display = "flex";
+      ASind.style.display = "none";
+      ASTeamCookie.style.display = "none";
+      coockieContainer.style.display = "none";
+      contactCookie.style.display = "none";
+    })
+  });
+  
